d3bd5fa9c891c529c81ff88cefc40d39908097c7
Bailey Cage <bailey.cage@gmail.com>
2023-06-01 11:52:46 -0400

    add carousel with Unsplash photos

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import { Col, Container, Row } from 'react-bootstrap'
 
 function Footer (props) {
   return (
-    <Container fluid style={{position: 'absolute', bottom: 0}}>
+    <Container fluid style={{position: 'absolute', bottom: 0, left: 0}}>
       <Row>
-        <Col>footer</Col>
+        <Col></Col>
+        <Col style={{textAlign: 'center'}}>Re:RE © 2023</Col>
+        <Col style={{textAlign: 'right'}}>Bailey Cage</Col>
       </Row>
     </Container>
   )
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,10 +1,56 @@
 import React from 'react'
-import { Container } from 'react-bootstrap'
+import { Carousel, Container, Image } from 'react-bootstrap'
+
+import { AUTH } from '../firebaseConfig'
+import { UNSPLASH_KEY, UNSPLASH_SECRET } from "../keys"
+
+const UNSPLASH_URL = "https://api.unsplash.com"
 
 function Home (props) {
+  const [images, setImages] = React.useState([])
+  const user = !!AUTH.currentUser ? AUTH.currentUser : null
+
+  React.useEffect(() => {
+    if (!!UNSPLASH_KEY && !!UNSPLASH_SECRET) {
+      const query = new URLSearchParams({
+        "collections": "4934758",
+        "orientation": "landscape",
+        "count": 5,
+        "client_id": UNSPLASH_KEY
+      })
+      fetch(UNSPLASH_URL + "/photos/random?" + query)
+        .then(res => {
+          if (res.ok) res.json().then(setImages)
+        })
+    }
+  }, [])
+
+  const carouselItems = !!images[0]
+    ? images.map(image => {
+      console.log(image)
+      return (
+        <Carousel.Item style={{maxHeight: 600}}>
+          <Image
+            fluid
+            className="mx-auto d-block"
+            src={image.urls.full}
+            alt={image.alt_description}
+          />
+          <Carousel.Caption>
+            <p>{image.user.name} via Unsplash</p>
+          </Carousel.Caption>
+        </Carousel.Item>
+      )
+    })
+    : null
+
   return (
-    <Container>
-      <h1>Re:RE</h1>
+    <Container fluid>
+      <Carousel style={{height: 600}}>
+        {carouselItems}
+      </Carousel>
+      <h1>Regarding: Real Estate</h1>
+      <p>Love browsing real estate but wish there was a comment feature? Now there is!</p>
     </Container>
   )
 }
